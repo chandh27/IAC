3fcdeea33f3d6af5f57aa4a4580746387473c9a8
chandh27 <103110895+chandh27@users.noreply.github.com>
2025-12-02 16:39:46 +0530

    Update main.tf

---
+++ terraform/modules/ec2/main.tf
@@ -0,0 +1,36 @@
+variable "public_subnet_id" {}
+variable "ec2_security_group_id" {}
+variable "instance_type" {
+  default = "t3.micro"
+}
+
+# Fetch latest Amazon Linux 2 AMI from SSM
+data "aws_ssm_parameter" "amzn2_latest" {
+  name = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ssm_parameter.amzn2_latest.value
+  instance_type = var.instance_type
+  subnet_id     = var.public_subnet_id
+  vpc_security_group_ids = [var.ec2_security_group_id]
+
+  associate_public_ip_address = true
+
+  user_data = <<-EOF
+    #!/bin/bash
+    yum update -y
+    yum install -y httpd
+    systemctl enable httpd
+    systemctl start httpd
+    echo "Hello from Terraform EC2 in Mumbai!" > /var/www/html/index.html
+  EOF
+
+  tags = {
+    Name = "terraform-web-ec2"
+  }
+}
+
+output "ec2_public_ip" {
+  value = aws_instance.web.public_ip
+}
